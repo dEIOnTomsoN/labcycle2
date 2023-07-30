1ead1864f73055ccf6287b9c8317744ba5ccf314
dEIOnTomsoN <125672061+dEIOnTomsoN@users.noreply.github.com>
2023-07-30 22:54:50 +0530

    Add files via upload

--- ar99.c
+++ ar99.c
@@ -1,20 +1,24 @@
 #include <stdio.h>
 int main()
 {
-    int a[10][10], b[10][10], mul[10][10], r, c, i, j, k;
+    int a[10][10], b[10][10], mul[10][10], r, c, i, j, k,h,l;
+    printf("The column of first matrix and Row of second matrix maust be same\n");
     printf("Enter the number of rows and columns of matrix\n");
     scanf("%d%d", &r, &c);
+    printf("Enter the number of rows and columns of matrix\n");
+    scanf("%d%d", &h, &l);
+  
     printf("Enter the elements of first matrix\n");
     for (i = 0; i < r; i++)
         for (j = 0; j < c; j++)
             scanf("%d", &a[i][j]);
     printf("Enter the elements of second matrix\n");
-    for (i = 0; i < r; i++)
-        for (j = 0; j < c; j++)
+    for (i = 0; i < h; i++)
+        for (j = 0; j < l; j++)
             scanf("%d", &b[i][j]);
     printf("Product of the matrices:\n");
     for (i = 0; i < r; i++) {
-        for (j = 0; j < c; j++) {
+        for (j = 0; j < l; j++) {
             mul[i][j] = 0;
             for (k = 0; k < c; k++)
                 mul[i][j] += a[i][k] * b[k][j];
@@ -22,4 +26,4 @@ int main()
         }
         printf("\n");
     }
-    }
+   }
